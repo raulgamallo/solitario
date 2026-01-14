1b8e1ec7cbbaabd49592534954416001cafb9e9e
mario <babayagapa@gmail.com>
2026-01-14 16:15:01 +0100

    solucionado

--- src/views/game.php
+++ src/views/game.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/game.css">
+    <link rel="stylesheet" href="../css/game.css">
     <title>Game</title>
 </head>
 
@@ -32,7 +32,7 @@
             <div id="tabla-6" class="column"></div>
         </div>
     </div>
-    <script src="/js/game.js"></script>
+    <script src="../js/game.js"></script>
 </body>
 
 </html>
